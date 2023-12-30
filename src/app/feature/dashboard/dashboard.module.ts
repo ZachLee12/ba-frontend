a0c10a4811ea2ef27c8d730bf23237ddbd876697
Zach Lee <leezhengyang22@gmail.com>
2023-12-30 23:14:38 +0100

    Add sidenav layout to home component

--- src/app/feature/dashboard/dashboard.module.ts
+++ src/app/feature/dashboard/dashboard.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,11 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
   ],
   imports: [
     DashboardRoutingModule,
-    CommonModule
+    CommonModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule
   ]
 })
 export class DashboardModule { }
