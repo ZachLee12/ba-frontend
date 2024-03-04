307def0f181a2f66e19e53637aaa8e4d9968010a
Zach Lee <leezhengyang22@gmail.com>
2024-03-04 22:06:14 +0100

    Display N/A when no users to approve

--- src/app/feature/admin/components/approve-users/approve-users.component.ts
+++ src/app/feature/admin/components/approve-users/approve-users.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { map, switchMap, take } from 'rxjs';
+import { Observable, map, switchMap, take, zip } from 'rxjs';
 import { AdminService } from 'src/app/core/services/admin/admin.service';
 import { ConfirmationDialogComponent } from 'src/app/feature/standalone/confirmation-dialog/confirmation-dialog.component';
 import { EmailVerification } from 'src/app/interfaces/resources.interfaces';
@@ -20,30 +20,28 @@ export class ApproveUsersComponent {
   dataSource: any[] = []
 
   ngOnInit() {
-    this.adminService.getAllUsers()
-      .pipe(
-        switchMap(users => {
-          return this.adminService.getAllEmailVerifications()
-            .pipe(map(emailVerifications => ({ emailVerifications, users })))
-        }),
-        take(1)
-      )
+    this.getEmailVerificationsAndUsersZip$()
       .subscribe(
         {
-          next: ({ emailVerifications, users }) => {
+          next: ([emailVerifications, users]) => {
             this.emailVerifications = emailVerifications
             this.columnNamesToDisplay = ['actions', 'username', 'verification_code', 'is_verified']
 
-            // Filter out users that have already been created
             const existingUsernames = users.map(user => user.username)
             // Display only users that have requested an account, but not yet approved.
             this.dataSource = emailVerifications.filter(object => !existingUsernames.includes(object.username)).map(object => ({ actions: '', ...object }))
+
             if (this.dataSource.length === 0) {
               this.dataSource = [{ action: '', username: 'No user email verifications', verification_code: 'N/A', is_verified: 'N/A' }]
             }
           }
         }
       )
+  }
+
+
+  getEmailVerificationsAndUsersZip$(): Observable<[EmailVerification[], { username: string, is_password_viewed: boolean }[]]> {
+    return zip(this.adminService.getAllEmailVerifications(), this.adminService.getAllUsers())
   }
 
   rejectUserAccountRequest(username: string) {
@@ -64,18 +62,26 @@ export class ApproveUsersComponent {
         }
       }
     )
+
     dialogRef.afterClosed().subscribe(confirmDelete => {
       if (confirmDelete) {
         this.adminService.rejectUserAccountRequest(username)
           .pipe(
-            switchMap(() => this.adminService.getAllEmailVerifications()),
-            take(1))
+            switchMap(() => this.getEmailVerificationsAndUsersZip$()),
+            take(1)
+          )
           .subscribe(
             {
-              next: emailVerifications => {
-                this.dataSource = []
-                if (!emailVerifications || emailVerifications.length < 1) {
-                  this.dataSource = emailVerifications.map(object => ({ actions: '', ...object }))
+              next: ([emailVerifications, users]) => {
+                this.emailVerifications = emailVerifications
+                this.columnNamesToDisplay = ['actions', 'username', 'verification_code', 'is_verified']
+
+                const existingUsernames = users.map(user => user.username)
+                // Display only users that have requested an account, but not yet approved.
+                this.dataSource = emailVerifications.filter(object => !existingUsernames.includes(object.username)).map(object => ({ actions: '', ...object }))
+
+                if (this.dataSource.length === 0) {
+                  this.dataSource = [{ action: '', username: 'No user email verifications', verification_code: 'N/A', is_verified: 'N/A' }]
                 }
               }
             }
@@ -83,5 +89,4 @@ export class ApproveUsersComponent {
       }
     })
   }
-
 }
